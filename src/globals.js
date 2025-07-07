edf5f59d83bffd0825a297a8453cf1f7033c4c0f
Noman5237 <anonyman637@gmail.com>
2025-07-07 10:06:34 +0600

    globals

--- src/globals.js
+++ src/globals.js
@@ -1,7 +1,7 @@
 export const GLOBALS = {
     PORT: process.env.PORT || 4000,
-    GITEA_URL: process.env.GITEA_URL || 'http://localhost:3000',
-    GITEA_TOKEN: process.env.GITEA_TOKEN || '57b4b92c0d7069275a50fa07c0fc4047ab7b0b02',
-    SONARQUBE_URL: process.env.SONARQUBE_URL || 'http://localhost:9000',
-    SONARQUBE_TOKEN: process.env.SONARQUBE_TOKEN || 'squ_21603ba435c62cecf014c26195e1bd51116c9b0c'
+    GITEA_URL: process.env.GITEA_URL || 'http://172.16.231.10:3000',
+    GITEA_TOKEN: process.env.GITEA_TOKEN || 'be30375fef80c4898ecd375c3c4ef731532e6a53',
+    SONARQUBE_URL: process.env.SONARQUBE_URL || 'http://172.16.231.10:9000',
+    SONARQUBE_TOKEN: process.env.SONARQUBE_TOKEN || 'squ_d33ee219b5ed4cf6509f8bc2a06b0ff1f9f4052a'
 }
